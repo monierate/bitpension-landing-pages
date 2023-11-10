243120d8348b05afdc41bae1f09eeb7622cde387
Heaven <gabrielheavenlydawn@gmail.com>
2023-11-10 10:00:58 +0100

    MDX Setup

---
+++ src/Posts/post.md
@@ -0,0 +1,8 @@
+---
+title: My First Post
+date: '2023-11-10'
+---
+
+# Welcome to my blog!
+
+First blog post using Next.js and MDX.
---
+++ src/components/MDXComponents.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MDXComponents = {
+  // Define your MDX components here
+  // Example:
+  h1: ({props}:any) => <h1 style={{ color: 'blue' }} {...props} />,
+  
+};
+
+export default MDXComponents;
